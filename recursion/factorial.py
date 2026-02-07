9038115eaf9fad8460d4255d66d5fb1e24ce40d0
madhushettar <madhushettar41@gmail.com>
2026-02-07 23:52:42 +0530

    Update and rename array_sum.py to factorial.py

---
+++ recursion/factorial.py
@@ -0,0 +1,8 @@
+# Recursive factorial calculation
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+if __name__ == "__main__":
+    print("Factorial of 5:", factorial(5))
